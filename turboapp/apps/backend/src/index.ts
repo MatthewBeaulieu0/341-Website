9d53f06548e381f51e96b8278f94104e9e26ad16
MatthewBeaulieu0 <matthew.beaulieu631@gmail.com>
2022-01-15 17:16:29 -0500

    Initial Commit

---
+++ turboapp/apps/backend/src/index.ts
@@ -0,0 +1,21 @@
+import express, { Application, Request, Response } from "express";
+//import jsonwebtoken from "jsonwebtoken";
+//import { hash, compare } from "bcrypt";
+import cors from "cors";
+const app: Application = express();
+
+const port: number = 3001; // Backend port
+app.use(express.json());
+app.use(
+    cors({
+        origin: ["http://localhost:4200"], // Origin frontend to change later when deployed
+    })
+);
+
+app.get("/api/hi", (_: Request, res: Response) => {
+    res.send("Hello WORLD!");
+});
+
+app.listen(port, () => {
+    console.log(`App is listening on port ${port} !`);
+});
