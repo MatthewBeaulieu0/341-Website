1f1c5f291c34f9cc49044dd3b3ec2a2be7c1160e
MatthewBeaulieu0 <matthew.beaulieu631@gmail.com>
2022-01-27 12:52:22 -0500

    Comment about post for #7

--- turboapp/apps/backend/src/index.ts
+++ turboapp/apps/backend/src/index.ts
@@ -11,7 +11,7 @@ app.use(
         origin: ["http://localhost:4200"], // Origin frontend to change later when deployed
     })
 );
-
+//For post please just add encryption
 app.get("/api/hi", (_: Request, res: Response) => {
     res.send("Hello WORLD!");
 });
