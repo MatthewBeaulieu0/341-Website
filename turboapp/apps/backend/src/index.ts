20dfe1bda8b1cad552857ed15eff815e1d8b9ec7
Mihai Olaru <mihai.c.olaru@gmail.com>
2022-01-27 17:19:36 -0500

    part of the web, related issue #23

--- turboapp/apps/backend/src/index.ts
+++ turboapp/apps/backend/src/index.ts
@@ -7,15 +7,17 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+  cors({
+    origin: ["http://localhost:4200"], // Origin frontend to change later when deployed
+  })
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-    res.send("Hello WORLD!"***REMOVED***
+  res.send("Hello WORLD!"***REMOVED***
 }***REMOVED***
 
 ***REMOVED***
-    console.log(`App is listening on port ${port} !`***REMOVED***
+  console.log(`App is listening on port ${port} !`***REMOVED***
 }***REMOVED***
+
+***REMOVED***
