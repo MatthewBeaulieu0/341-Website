46859dab9858f8089de41b8582ab8f8693628b62
Jonathan-Riso <riso.jona10@gmail.com>
2022-02-01 09:38:29 -0500

    created schema for products

--- turboapp/apps/backend/src/models/products.ts
+++ turboapp/apps/backend/src/models/products.ts
@@ -1,1 +1,12 @@
-import * as yup from 'yup';
+let yup = require('yup'***REMOVED***
+
+export const product_schema = yup.object().shape({
+  name: yup.string().required(),
+  desc: yup.string(),
+  price: yup.number().required().positive(),
+  currency: yup.string().default('CAD'),
+  brand: yup.string(),
+  seller: yup.string(),
+  other: yup.object(),
+  stock: yup.number().required().integer()
+}***REMOVED***
