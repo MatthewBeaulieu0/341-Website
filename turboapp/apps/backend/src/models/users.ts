e2dcb5086e1457debaf11defacb3979a590dac9e
Jonathan-Riso <riso.jona10@gmail.com>
2022-02-01 09:50:03 -0500

    created the users schema

--- turboapp/apps/backend/src/models/users.ts
+++ turboapp/apps/backend/src/models/users.ts
@@ -1,1 +1,14 @@
-import * as yup from 'yup';
+let yup = require('yup'***REMOVED***
+
+enum type_enum {
+    BUYER = 'buyer',
+    SELLER = 'seller'
+}; 
+
+export const user_schema = yup.object().shape({
+    name: yup.string().required(type_enum),
+    type: yup.string().oneOf(),
+    age: yup.number().required().positive().integer(),
+    email: yup.string().email(),
+    address: yup.string().address()
+  }***REMOVED***
