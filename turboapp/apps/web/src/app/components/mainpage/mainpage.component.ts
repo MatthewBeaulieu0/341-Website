df9e4da0273aa0644a0d993a667d1e9f9990d4fb
Niniishere <annieliu190@gmail.com>
2022-02-07 08:32:02 -0500

    pull changes on the main page branch

--- turboapp/apps/web/src/app/components/mainpage/mainpage.component.ts
+++ turboapp/apps/web/src/app/components/mainpage/mainpage.component.ts
@@ -1,7 +1,6 @@
 import { Identifiers } from '@angular/compiler';
 import { Component, OnInit, ViewChild, ElementRef, ViewChildren, QueryList, Input } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { NgbCarousel, NgbSlideEvent, NgbSlideEventSource} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-mainpage',
