59c50ef30d4a74f9692b74683f671ad0100860ce
Niniishere <annieliu190@gmail.com>
2022-02-07 20:03:14 -0500

    added the footer to the website

---
+++ turboapp/apps/web/src/app/components/productpage/productpage.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProductpageComponent } from './productpage.component';
+
+describe('ProductpageComponent', () => {
+  let component: ProductpageComponent;
+  let fixture: ComponentFixture<ProductpageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ProductpageComponent ]
+***REMOVED***
+    .compileComponents(***REMOVED***
+  }***REMOVED***
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProductpageComponent***REMOVED***
+    component = fixture.componentInstance;
+    fixture.detectChanges(***REMOVED***
+  }***REMOVED***
+
+  it('should create', () => {
+    expect(component).toBeTruthy(***REMOVED***
+  }***REMOVED***
+}***REMOVED***
