182b9e09bfdb3c56de8dc87d864cece04496a5e2
AnnaHsu1 <annahsu2000@hotmail.com>
2022-02-12 19:34:49 -0500

    Created the shopping cart page. Added new Angular component

---
+++ turboapp/apps/web/src/app/components/shoppingcart/shoppingcart.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ShoppingcartComponent } from './shoppingcart.component';
+
+describe('ShoppingcartComponent', () => {
+  let component: ShoppingcartComponent;
+  let fixture: ComponentFixture<ShoppingcartComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ShoppingcartComponent ]
+***REMOVED***
+    .compileComponents(***REMOVED***
+  }***REMOVED***
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ShoppingcartComponent***REMOVED***
+    component = fixture.componentInstance;
+    fixture.detectChanges(***REMOVED***
+  }***REMOVED***
+
+  it('should create', () => {
+    expect(component).toBeTruthy(***REMOVED***
+  }***REMOVED***
+}***REMOVED***
