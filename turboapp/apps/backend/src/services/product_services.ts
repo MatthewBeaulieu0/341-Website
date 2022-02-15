9629245630af957a0903ec60df6c2072960dcc12
Jonathan-Riso <riso.jona10@gmail.com>
2022-02-14 21:45:10 -0500

    Implemented batch find products for shopping cart

--- turboapp/apps/backend/src/services/product_services.ts
+++ turboapp/apps/backend/src/services/product_services.ts
@@ -24,6 +24,18 @@ export async function find_product_by_id(product_id: string) {
     return product;
 }
 
+export async function batch_find_products_by_ids(product_ids: Array<string>) {
+    var sql = `SELECT * FROM products WHERE ProductID IN ?`;
+    try {
+        var products = await query(sql, [product_ids]);
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+
+    return products;
+}
+
 export async function create_product(product: any) {
     var sql = "INSERT INTO products (??) VALUES (?)";
     var params = [];
