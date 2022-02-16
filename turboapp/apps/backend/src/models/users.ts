972e83410e42b719f38710c2c4b1cc9b0d1a3d2a
Jonathan-Riso <riso.jona10@gmail.com>
2022-02-16 16:36:35 -0500

    Implemented more services and updated users schema

--- turboapp/apps/backend/src/models/users.ts
+++ turboapp/apps/backend/src/models/users.ts
@@ -2,6 +2,7 @@ import * as yup from 'yup';
 
 export const user_schema = yup.object().shape({
     name: yup.string().required(),
+    password: yup.string().required(),
     type: yup.string().oneOf(['buyer', 'seller']),
     age: yup.number().required().positive().integer(),
     email: yup.string().email(),
--- turboapp/apps/backend/src/services/user_services.ts
+++ turboapp/apps/backend/src/services/user_services.ts
@@ -1,3 +1,6 @@
+import { query } from "../helpers/query_helper";
+import { batch_find_products_by_ids } from "./product_services";
+
 let user_db: any = {
     "jim_1": {
         "name": "jim",
@@ -30,4 +33,58 @@ export function create_user(user: any){
         return null;
     }
     return user_db[user.name];
+}
+
+export function add_to_user_cart(user_id: number, product_id: number){
+    var sql =   `
+                    UPDATE users
+                    SET shopping_cart=JSON_ARRAY_APPEND(shopping_cart, '$', ?)
+                    WHERE UserID = ?;
+                `;
+    try {
+        let product_ids: any = await query(sql, [product_id, user_id]);
+        var products = batch_find_products_by_ids(product_ids);
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+
+    return products;
+}
+
+export async function remove_from_user_cart(user_id: number, product_id: number){
+    var sql =`
+                UPDATE users
+                SET shopping_cart=JSON_ARRAY_APPEND(??)
+                WHERE UserID = ?;
+            `;
+    try {
+        let product_ids: any = await get_user_cart(user_id)
+        const index = product_ids.indexOf(product_id);
+        if (index == -1){
+            return null;
+        }
+        else{
+            product_ids.splice(index, 1)
+            var result = await query(sql, [product_ids, user_id]) 
+        }
+    } catch (error) {
+    console.log(error);
+    throw error;
+}
+
+return result;
+}
+
+export async function get_user_cart(user_id: number){
+    var sql = `SELECT (shopping_cart) FROM users WHERE UserID = ?;`;
+    try {
+        let product_ids: any = await query(sql, [user_id]);
+        var products = batch_find_products_by_ids(product_ids);
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+
+    return products;
 }
