627601582042f6c1a051173b699072e7e870482a
dianaoana1 <dianamerlusca2001@gmail.com>
2022-02-17 10:47:46 -0500

    separated page components from smaller components

--- turboapp/apps/web/src/app/app-routing.module.ts
+++ turboapp/apps/web/src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MainpageComponent } from './components/mainpage/mainpage.component';
-import { ProductpageComponent } from './components/productpage/productpage.component';
-import { ShoppingcartComponent } from './components/shoppingcart/shoppingcart.component';
+import { MainpageComponent } from './components/pages/mainpage/mainpage.component';
+import { ProductpageComponent } from './components/pages/productpage/productpage.component';
+import { ShoppingcartComponent } from './components/pages/shoppingcart/shoppingcart.component';
 
 const routes: Routes = [
   {path: "", redirectTo: "mainpage", pathMatch: "full"},
