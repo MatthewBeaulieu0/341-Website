897251930a227489098d75171a702ab654b5e402
MatthewBeaulieu0 <matthew.beaulieu631@gmail.com>
2022-03-03 17:05:12 -0500

    Database should now be updated with the differetn relations

---
+++ setup/DatabaseCreationCode.sql
@@ -0,0 +1,80 @@
+USE fake_amazon;
+CREATE TABLE `User` (
+    `user_id` int  NOT NULL AUTO_INCREMENT ,
+    `Name` varchar(45)  NOT NULL ,
+    -- Not Null
+    `password` varchar(45)  NOT NULL ,
+    -- t or f for seller
+    `seller` boolean  NOT NULL ,
+    -- Not Null
+    `age` int  NOT NULL ,
+    `email` varchar(45)  NOT NULL ,
+    `address` varchar(45)  NOT NULL ,
+    PRIMARY KEY (
+        `user_id`
+    )
+)ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+  
+CREATE TABLE `Order` (
+    `order_id` int  NOT NULL AUTO_INCREMENT ,
+    `user_id` int  NOT NULL ,
+    `total_amount` int  NOT NULL ,
+    `order_status_id` int  NOT NULL ,
+    PRIMARY KEY (
+        `order_id`
+    )
+);
+
+CREATE TABLE `OrderLine` (
+    `order_line_id` int  NOT NULL AUTO_INCREMENT,
+    `order_id` int  NOT NULL ,
+    `product_id` int  NOT NULL ,
+    `Quantity` int  NOT NULL ,
+    PRIMARY KEY (
+        `order_line_id`
+    )
+);
+
+CREATE TABLE `Product` (
+    `product_id` int  NOT NULL AUTO_INCREMENT ,
+    `Name` varchar(45)  NOT NULL ,
+    `description` longtext  NOT NULL ,
+    `Price` int  NOT NULL ,
+    `brand` varchar(45)  NOT NULL ,
+    `seller` varchar(45)  NOT NULL ,
+    `stock` int  NOT NULL ,
+    `link` varchar(180)  NOT NULL ,
+    PRIMARY KEY (
+        `product_id`
+    ),
+    CONSTRAINT `uc_Product_Name` UNIQUE (
+        `Name`
+    )
+);
+
+CREATE TABLE `OrderStatus` (
+    `order_status_id` int  NOT NULL AUTO_INCREMENT ,
+    -- Finished etc
+    `Status` varchar(45)  NOT NULL ,
+    PRIMARY KEY (
+        `order_status_id`
+    ));
+
+
+ALTER TABLE `Order` ADD CONSTRAINT `fk_Order_user_id` FOREIGN KEY(`user_id`)
+REFERENCES `User` (`user_id`);
+
+ALTER TABLE `Order` ADD CONSTRAINT `fk_Order_order_status_id` FOREIGN KEY(`order_status_id`)
+REFERENCES `OrderStatus` (`order_status_id`);
+
+ALTER TABLE `OrderLine` ADD CONSTRAINT `fk_OrderLine_order_id` FOREIGN KEY(`order_id`)
+REFERENCES `Order` (`order_id`);
+
+ALTER TABLE `OrderLine` ADD CONSTRAINT `fk_OrderLine_product_id` FOREIGN KEY(`product_id`)
+REFERENCES `Product` (`product_id`);
+
+CREATE INDEX `idx_User_Name`
+ON `User` (`Name`);
+
