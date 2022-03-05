85aba71565af76a025b22dca6d65ceb121444270
dianaoana1 <dianamerlusca2001@gmail.com>
2022-03-05 16:39:58 -0500

    added regex validation for sign up page

--- turboapp/apps/web/src/app/components/pages/signuppage/signuppage.component.ts
+++ turboapp/apps/web/src/app/components/pages/signuppage/signuppage.component.ts
@@ -10,11 +10,43 @@ export class SignuppageComponent implements OnInit {
   constructor() { }
 
   onClickSubmit(data){
-    console.log(data.firstname);
-    console.log(data.lastname);
-    console.log(data.email);
-    console.log(data.number);
-    console.log(data.password);
+
+    //verifying first name
+    if(/^[a-z ,.'-]+$/i.test(data.firstname)){
+      // add to db
+      console.log(data.firstname);
+    } else {
+      console.log("invalid first name");
+    }
+
+    // verifying last name
+    if(/^[a-z ,.'-]+$/i.test(data.lastname)){
+      // add to db
+      console.log(data.lastname);
+    } else {
+      console.log("invalid last name");
+    }
+
+    if(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(data.email)){
+      // add to db
+      console.log(data.email);
+    } else {
+      console.log("invalid email address");
+    }
+
+    if(/^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/.test(data.number)){
+      // add to db
+      console.log(data.number);
+    } else {
+      console.log("invalid number");
+    }
+
+    if(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/.test(data.password)){
+      //add to db
+      console.log(data.password)
+    } else {
+      console.log("Password must have at least 8 characters, at least 1 letter and 1 number");
+    }
   }
 
   ngOnInit(): void {
