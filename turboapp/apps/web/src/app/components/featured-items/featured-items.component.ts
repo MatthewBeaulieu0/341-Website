7fb4893f41f4098fd5a4aa9be4a932dace03fc66
dianaoana1 <dianamerlusca2001@gmail.com>
2022-03-07 16:12:28 -0500

    added featured items from database

--- turboapp/apps/web/src/app/components/featured-items/featured-items.component.ts
+++ turboapp/apps/web/src/app/components/featured-items/featured-items.component.ts
@@ -11,25 +11,34 @@ import { MainpageComponent } from '../pages/mainpage/mainpage.component';
   styleUrls: ['./featured-items.component.css'],
 })
 export class FeaturedItemsComponent
-  extends MainpageComponent
   implements OnInit
 {
-  override products = this.getProductArray();
-
-  productPageRoute() {
-    this.router.navigate(['/productpage']);
-  }
+  featuredItems: Product[] = [];
 
   constructor(
-    // private route: ActivatedRoute,
-    // private cartService: CartService,
-    // private httpClient: HttpClient
-    protected override router: Router,
-    protected override activatedRoute: ActivatedRoute,
-    protected override httpClient: HttpClient
+    private route: ActivatedRoute,
+    private cartService: CartService,
+    private httpClient: HttpClient
   ) {
-    super(router, activatedRoute, httpClient);
   }
 
-  override ngOnInit(): void {}
+  getFeaturedItems() {
+    this.httpClient
+      .get<any>('http://localhost:3001/product/filter/products?name=')
+      .subscribe((response) => {
+        console.log(response[1]);
+        console.log(response[1].length);
+        for(let i = 0; i < response[1].length; i++){
+          if(response[1][i].featured == 1)
+          this.featuredItems.push(response[1][i]);
+          console.log(this.featuredItems);
+        }
+        console.log('This product' + JSON.stringify(this.featuredItems[0].name));
+        console.log('This product' + JSON.stringify(this.featuredItems[0].price));
+      });
+  }
+
+  ngOnInit(): void {
+    this.getFeaturedItems();
+  }
 }
