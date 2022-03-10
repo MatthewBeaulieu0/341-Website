23bbf049c7d314ce2a3bae8671fdc561c196794b
AnnaHsu1 <annahsu2000@hotmail.com>
2022-03-09 19:12:53 -0500

    Changed wording in login page

--- turboapp/apps/web/src/app/components/pages/loginpage/loginpage.component.ts
+++ turboapp/apps/web/src/app/components/pages/loginpage/loginpage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-loginpage',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginpageComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+    ) { }
 
   ngOnInit(): void {
+  }
+
+  routeToSignUpPage(){
+    this.router.navigate(['/signup']);
   }
 
 }
