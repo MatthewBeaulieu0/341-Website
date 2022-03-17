d6187c826519019c970cf91898044b9debc91065
AnnaHsu1 <annahsu2000@hotmail.com>
2022-03-17 19:47:44 -0400

    Implemented reactive form validation for login

--- turboapp/apps/web/src/app/components/pages/loginpage/loginpage.component.ts
+++ turboapp/apps/web/src/app/components/pages/loginpage/loginpage.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl,FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { User } from 'src/app/models/user';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-loginpage',
@@ -9,14 +13,25 @@ import { Router } from '@angular/router';
 export class LoginpageComponent implements OnInit {
 
   constructor(
-    private router: Router
+    private router: Router,
+    private httpClient: HttpClient
     ) { }
 
+    exform: FormGroup;
+
   ngOnInit(): void {
+    this.exform = new FormGroup({ 
+    email: new FormControl( null, [Validators.required, Validators.email]),
+    password: new FormControl( null, [Validators.required, Validators.pattern("^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$")])
+    })
   }
 
   routeToSignUpPage(){
     this.router.navigate(['/signup']);
+  }
+
+  routeToMainPage(){
+    this.router.navigate(['/mainpage']);
   }
 
 }
