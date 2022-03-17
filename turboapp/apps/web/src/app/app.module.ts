ac84e98eb4a531ff96a160a2bb3d7377e2904d05
AnnaHsu1 <annahsu2000@hotmail.com>
2022-03-17 19:46:29 -0400

    Imported ReactiveFormsModule

--- turboapp/apps/web/src/app/app.module.ts
+++ turboapp/apps/web/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -42,9 +42,9 @@ import { CheckoutpageComponent } from './components/pages/checkoutpage/checkoutp
     ProductaisleComponent,
     SignupNavbarComponent,
     LoginpageComponent,
-    CheckoutpageComponent
+    CheckoutpageComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, NgbModule, FormsModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, NgbModule, FormsModule, HttpClientModule, ReactiveFormsModule],
   providers: [CartService, ProductsService],
   bootstrap: [AppComponent],
 })
