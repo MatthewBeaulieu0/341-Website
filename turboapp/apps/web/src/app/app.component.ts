592c5dbc0135f391604c52aff5537f4e535f3195
RandomKingRD <rohandas@live.ca>
2022-03-26 21:28:37 -0400

    Fixed global login issue

--- turboapp/apps/web/src/app/app.component.ts
+++ turboapp/apps/web/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
+import { AuthService } from './auth/auth.service';
+import { LoginStatusService } from './services/login-status.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  //providers: [AuthService, LoginStatusService]
 })
-export class AppComponent {
+export class AppComponent  implements AfterViewInit{
+
   title = 'web';
+  constructor(private authService: AuthService, private statusService:LoginStatusService){
+    
+  }
+  ngAfterViewInit(): void {
+    //this.statusService.setLoginStatus(this.authService.isAuthenticated());
+    this.statusService.setLoginStatus(true);
+  }
+ 
+  
+
 }
+
