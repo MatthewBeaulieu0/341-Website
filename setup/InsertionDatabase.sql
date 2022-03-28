e9ecaa82104371a38c1835c513409591451a91ce
Jonathan-Riso <riso.jona10@gmail.com>
2022-03-28 12:03:21 -0400

    updated sql insertion in the setup

--- setup/InsertionDatabase.sql
+++ setup/InsertionDatabase.sql
@@ -1,11 +1,11 @@
 INSERT INTO `product` (`Name`,`Description`,`Price`,`Brand`,`Seller`,`Stock`, `Link`,`Category`)
 VALUES
-  ("Banana","yellow fruit",0.99,"Fake Amazon", "Fake Amazon",5, "/assets/images/banana.jpeg, /assets/images/banana1.jpeg","Food"),
-  ("Calculator","math tools",39.99,"CASIO", "Fake Amazon",20, "/assets/images/calculator.jpeg" ,"Electronic"),
-  ("Chocolate","tasty chocolate",3.98,"Kit Kat", "Fake Amazon",30, "/assets/images/chocalate.jpeg","Food"),
-  ("Strawberry","red fruit", 4.99,"Fake Amazon", "South American Business ",6272, "/assets/images/strawberry.jpeg","Food" ),
-  ("Bookshelf","wooden object that can hold books",249.99,"Fake Amazon", "Costco",29, "/assets/images/bookshelf.jpeg","Furniture"),
-  ("Phone","Electrical device that allows you to complete several tasks", 599.99,"Amazon", "Fake Amazon",92933, "/assets/images/phone.jpeg","Electronic");
+  ("Banana","yellow fruit",0.99,"Fake Amazon", "Fake Amazon",5, "/assets/images/banana.jpeg, /assets/images/banana1.jpeg, /assets/images/banana2.jpeg, /assets/images/banana3.jpeg","Food"),
+  ("Calculator","math tools",39.99,"CASIO", "Fake Amazon",20, "/assets/images/calculator.jpeg, /assets/images/calculator1.jpeg, /assets/images/calculator2.jpeg, /assets/images/calculator3.jpeg" ,"Electronic"),
+  ("Chocolate","tasty chocolate",3.98,"Kit Kat", "Fake Amazon",30, "/assets/images/chocalate.jpeg, /assets/images/chocalate1.jpeg, /assets/images/chocalate2.jpeg, /assets/images/chocalate3.jpeg","Food"),
+  ("Strawberry","red fruit", 4.99,"Fake Amazon", "South American Business ",6272, "/assets/images/strawberry.jpeg, /assets/images/strawberry1.jpeg, /assets/images/strawberry2.jpeg, /assets/images/strawberry3.jpeg","Food" ),
+  ("Bookshelf","wooden object that can hold books",249.99,"Fake Amazon", "Costco",29, "/assets/images/bookshelf.jpeg, /assets/images/bookshelf1.jpeg, /assets/images/bookshelf2.jpeg, /assets/images/bookshelf3.jpeg","Furniture"),
+  ("Phone","Electrical device that allows you to complete several tasks", 599.99,"Amazon", "Fake Amazon",92933, "/assets/images/phone.jpeg, /assets/images/phone1.jpeg, /assets/images/phone2.jpeg, /assets/images/phone3.jpeg","Electronic");
 
 
 INSERT INTO `User` (`Name`,`password`, `seller`,`age`,`email`,`address`)
