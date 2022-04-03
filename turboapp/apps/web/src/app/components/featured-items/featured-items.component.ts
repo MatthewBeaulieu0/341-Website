9f710e44a56914d7a3a85d6dedadcee9192131c6
mihaicolaru <mihai770o@gmail.com>
2022-04-02 20:44:20 -0400

    fixed product prage add to cart bug

--- turboapp/apps/web/src/app/components/featured-items/featured-items.component.ts
+++ turboapp/apps/web/src/app/components/featured-items/featured-items.component.ts
@@ -5,11 +5,12 @@ import { CartService } from 'src/app/services/cart.service';
 import { HttpClient } from '@angular/common/http';
 import { FeaturedItemsService } from 'src/app/services/featured-items.service';
 import { ProductsService } from 'src/app/services/products.service';
+import { GlobalUserService } from 'src/app/services/global-user.service';
 
 @Component({
   selector: 'app-featured-items',
   templateUrl: './featured-items.component.html',
-  styleUrls: ['./featured-items.component.css']
+  styleUrls: ['./featured-items.component.css'],
 })
 export class FeaturedItemsComponent implements OnInit {
 
@@ -22,7 +23,9 @@ export class FeaturedItemsComponent implements OnInit {
   constructor(
     private _featuredItemsService: FeaturedItemsService,
     private _productsService: ProductsService,
-    private router: Router
+    private router: Router,
+    private globalUserService: GlobalUserService,
+    private httpClient: HttpClient
   ) {}
 
   sendProductID(id: number) {
@@ -45,5 +48,20 @@ export class FeaturedItemsComponent implements OnInit {
       console.log('This product' + JSON.stringify(this.featuredItems[0].name));
       console.log('This product' + JSON.stringify(this.featuredItems[0].price));
     });
+  }
+
+  addToCart(product: Product) {
+    console.log(product);
+    const body = {
+      product_id: product.product_id,
+    };
+    this.httpClient
+      .put('http://localhost:3001/user/id/' + this.globalUserService.getNewUser().user_id +  '/shopping_cart', body, {
+        responseType: 'text',
+        headers: { 'content-type': 'application/json' },
+      })
+      .subscribe((s) => {
+        console.log(s);
+      });
   }
 }
