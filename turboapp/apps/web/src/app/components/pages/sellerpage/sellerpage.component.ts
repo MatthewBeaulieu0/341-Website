346af860369e01ba76ad7856a0e038799c490618
Niniishere <annieliu190@gmail.com>
2022-04-03 18:00:45 -0400

    seller page added

---
+++ turboapp/apps/web/src/app/components/pages/sellerpage/sellerpage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sellerpage',
+  templateUrl: './sellerpage.component.html',
+  styleUrls: ['./sellerpage.component.css']
+})
+export class SellerpageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
