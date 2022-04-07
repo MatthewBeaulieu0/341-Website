51a9d27c85d920189192f1e93ca775dbd58a14f8
AnnaHsu1 <annahsu2000@hotmail.com>
2022-04-06 20:07:40 -0400

    Added pattern validation for category field

--- turboapp/apps/web/src/app/components/pages/sellerpage/sellerpage.component.ts
+++ turboapp/apps/web/src/app/components/pages/sellerpage/sellerpage.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Product } from 'src/app/models/product';
+import { backendProduct } from 'src/app/models/backendProduct';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
@@ -12,17 +12,17 @@ export class SellerpageComponent implements OnInit {
 
   constructor(private httpClient: HttpClient, private router: Router) { }
 
-  product: Product = {
-    product_id: 0,
+  product : backendProduct = {
     name: '',
     description: '',
     price: 0,
+    other: null,
+    stock: 0,
     brand: '',
     seller: '',
-    stock: 0,
-    link: '',
-    featured: false,
-    link_array: null,
+    link: "src/assets/images/banana.jpeg",
+    category: ''
+
   };
 
   ngOnInit(): void {
@@ -33,7 +33,7 @@ export class SellerpageComponent implements OnInit {
   }
   
 
-  onClickSubmit(data){
+  onClickSeller(data){
 
     //verifying item name
     if (/^[A-Za-z ,.'-]+$/i.test(data.name)) {
@@ -76,12 +76,20 @@ export class SellerpageComponent implements OnInit {
       console.log('invalid seller');
     }
 
-    // verifying price
+    // verifying stock
     if (/^[0-9]+$/i.test(data.stock)) {
       // add to db
       console.log(data.stock);
     } else {
       console.log('invalid stock');
+    }
+
+    // verifying category
+    if (/^[A-Za-z ,.'-]+$/i.test(data.category)) {
+      // add to db
+      console.log(data.category);
+    } else {
+      console.log('invalid category');
     }
 
     if(
@@ -90,7 +98,8 @@ export class SellerpageComponent implements OnInit {
       /^[0-9]+$/i.test(data.price) &&
       /^[A-Za-z 0-9 ,.'-]+$/i.test(data.brand) &&
       /^[A-Za-z ,.'-]+$/i.test(data.seller) &&
-      /^[0-9]+$/i.test(data.stock)
+      // /^[0-9]+$/i.test(data.stock) &&
+      /^[A-Za-z ,.'-]+$/i.test(data.category)
     )
     //testing all the conditions before sending to backend
     {
@@ -99,7 +108,8 @@ export class SellerpageComponent implements OnInit {
       let price: number = data.price;
       let brand: string = data.brand;
       let seller: string = data.seller;
-      let stock: number = data.stock;  
+      let stock: number = data.stock;
+      let category: string = data.category;  
 
       this.product.name = itemName;
       this.product.description = description;
@@ -107,19 +117,23 @@ export class SellerpageComponent implements OnInit {
       this.product.brand = brand;
       this.product.seller = seller;
       this.product.stock = stock;
+      this.product.category = category;
 
       console.log(this.product);
+      console.log(typeof this.product.stock);
 
       //Send HTTP request
       const body = {
         product: this.product,
       };
+      const options = {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json',
+        }),
+      };
 
       this.httpClient
-        .post('http://localhost:3001/product', body, {
-          responseType: 'text',
-          headers: { 'content-type': 'application/json' },
-        })
+        .post('http://localhost:3001/product/', body, options)
         .subscribe((s) => {
           console.log(s);
         });
